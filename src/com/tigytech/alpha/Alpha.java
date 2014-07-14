2bfa2cf79bb72b117aafc08274bb443d1d3cdf0d
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2014-07-13 21:42:54 -0400

    Update Alpha.java

--- src/com/tigytech/alpha/Alpha.java
+++ src/com/tigytech/alpha/Alpha.java
@@ -1,32 +1,5 @@
 package com.tigytech.alpha;
 
-/**
- * 									 ,MMM
- * 								    MM  MM
- * 									   ,MM
- * 									 ,MM'
- * 									MM`
- * 									MMMMMM
- *              MMMMMMM      MMMM          
- *           MMMN    MMM   MMMM           
- *         MMM        MM  MMM            
- *        MMMM        MM MMMM            
- *       MMMM          MMMMM            
- *       MMMM          MMMMM             
- *       MMMM          MMMM              
- *       MMMM          MMMN              
- *       MMMM          MMM               
- *        MMMM       .MMMMM    M         
- *        MMMM       MMM MMM   M        
- *         MMMM     MMM.  MMMMMM        
- *           MMMMMMMM      MMMM         
- *                                     
- *     [ a l p h a - a n a l y s i s ]
- * 
- * @author phi
- * @version alpha
- */
-
 public class Alpha {
 	
 	public static void main(String[] args) {
